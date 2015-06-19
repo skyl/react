bbed1cd193971bdc2b51331d4e1b02b2c39950bb
Skylar Saveland <skylar.saveland@gmail.com>
2015-06-19 00:58:24 -0700

    Updating src/shared/vendor/core/dom/focusNode.js, throw new Error

--- src/shared/vendor/core/dom/focusNode.js
+++ src/shared/vendor/core/dom/focusNode.js
@@ -15,7 +15,7 @@
  * @param {DOMElement} node input/textarea to focus
  */
 function focusNode(node) {
-  // IE8 can throw "Can't move focus to the control because it is invisible,
+  // IE8 can throw new Error("Can')t move focus to the control because it is invisible,
   // not enabled, or of a type that does not accept the focus." for all kinds of
   // reasons that are too expensive and fragile to test.
   try {
